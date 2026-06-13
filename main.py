c19581e033702d9d208d09b0296a2ec01b53cfe1
Muhammad Zaid Zia <zaidzia46@gmail.com>
2026-06-13 16:42:20 +0500

    testing deployment

--- main.py
+++ main.py
@@ -1,33 +1,33 @@
 from fastapi import FastAPI, Depends
-from fastapi.middleware.cors import CORSMiddleware
-from auth.router import router as auth_router
-from config import FRONTEND_ADMIN_URL, FRONTEND_APP_URL
-from students.router import router as students_router
-from admin.router import public_router as admin_public_router
-from admin.router import protected_router as admin_protected_router
-from models import models
-from db.session import engine
-from core.security import router as refresh_router
-from fastapi.staticfiles import StaticFiles
+# from fastapi.middleware.cors import CORSMiddleware
+# from auth.router import router as auth_router
+# from config import FRONTEND_ADMIN_URL, FRONTEND_APP_URL
+# from students.router import router as students_router
+# from admin.router import public_router as admin_public_router
+# from admin.router import protected_router as admin_protected_router
+# from models import models
+# from db.session import engine
+# from core.security import router as refresh_router
+# from fastapi.staticfiles import StaticFiles
 
 app = FastAPI(title="CampusGPT API", description="API for CampusGPT application", version="1.0.0")
-app.mount('/static-pwd-reset', StaticFiles(directory='static-pwd-reset'), name='static-pwd-reset')
+# app.mount('/static-pwd-reset', StaticFiles(directory='static-pwd-reset'), name='static-pwd-reset')
 
-app.include_router(auth_router)
-app.include_router(students_router)
-app.include_router(refresh_router)
-app.include_router(admin_public_router)
-app.include_router(admin_protected_router)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+# app.include_router(auth_router)
+# app.include_router(students_router)
+# app.include_router(refresh_router)
+# app.include_router(admin_public_router)
+# app.include_router(admin_protected_router)
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=["*"],
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 @app.get('/')
 def getme():
     return{'message': 'API connects'}
 
-models.base.metadata.create_all(bind=engine)
+# models.base.metadata.create_all(bind=engine)
